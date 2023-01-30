366821ffbad1f279348b32b67b07ab296ed91b05
Ramanishka Kanstantsin <43388386+kostiqq@users.noreply.github.com>
2023-01-30 23:41:27 +0300

    Add spawner settings

---
+++ Assets/Scripts/StaticData/SpawnerSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace StaticData
+{
+    [CreateAssetMenu(fileName = "SpawnerSettings", menuName = "Config/SpawnerSettings", order = 0)]
+    public class SpawnerSettings : ScriptableObject
+    {
+        public int StartFrequency;
+        public float DifficultMultiplyer;
+    }
+}
