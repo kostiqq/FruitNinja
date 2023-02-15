3bc0caee3dc83ea34f8bb4bf5a4c260b121f7ad3
Kanstantsin Ramanishka <kqqwm1@gmail.com>
2023-02-15 23:44:56 +0300

    Destroy animation for heart UI

--- Assets/Scripts/UI/GameView/Heart.cs
+++ Assets/Scripts/UI/GameView/Heart.cs
@@ -3,11 +3,15 @@ using UnityEngine.UI;
 
 public class Heart : MonoBehaviour
 {
+    private static readonly int Die = Animator.StringToHash("Die");
+    
     [SerializeField] private Image leftPart;
     [SerializeField] private Image rightPart;
-
-    public void DestroyAnimation()
-    {
+    [SerializeField] private Animator animator;
+    
+    public void DestroyAnimation()=>
+        animator.SetTrigger(Die);
+    
+    private void OnDisable()=>
         Destroy(gameObject);
-    }
 }
